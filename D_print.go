1027dbdf7a3cd790fdf7875210ca27461d197ba7
congxia wu <1398173038@qq.com>
2023-11-11 18:29:13 +0800

    v0.0.0

---
+++ D_print.go
@@ -0,0 +1,15 @@
+package D_gomodule
+
+import (
+	"fmt"
+	"github.com/deer7777/E/v2"
+)
+
+/*func main() {
+	D_print()
+}*/
+
+func D_print() {
+	fmt.Println("version D_v0.0.0")
+	E.E_print()
+}
---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/deer7777/D_gomodule
+
+go 1.21.1
+
+require github.com/deer7777/E/v2 v2.0.0
+
+//require github.com/deer7777/B/v2 v2.0.0
