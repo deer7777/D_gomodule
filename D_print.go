d1b2c29531ce4c9c1d1433be40a2fa681e1b2c19
deer <1398173038@qq.com>
2023-11-22 14:45:34 +0800

    v1.0.0

--- D_print.go
+++ D_print.go
@@ -2,14 +2,8 @@ package D_gomodule
 
 import (
 	"fmt"
-	"github.com/deer7777/E/v2"
 )
 
-/*func main() {
-	D_print()
-}*/
-
 func D_print() {
-	fmt.Println("version D_v0.0.0")
-	E.E_print()
+	fmt.Println("version D_v1.0.0")
 }
---
+++ sub_D/sub_D.go
@@ -0,0 +1,7 @@
+package sub_D
+
+import "fmt"
+
+func sub_Dprint() {
+	fmt.Println("sub_D version v1.0.0")
+}
